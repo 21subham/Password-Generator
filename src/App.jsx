8edad8ceba722212bb06cba6b9705f7c95d37b7b
21subham <subhamrajbhandari@gmail.com>
2024-11-09 22:56:54 +0545

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [length, setlength] = useState(8);
+  const [num, setnum] = useState(false);
+  const [char, setchar] = useState(false);
   return (
     <>
-      <h1 className="text-4xl text-center">Password Generator</h1>
+      <h1 className="text-4xl text-center text-white">Password Generator</h1>
     </>
   );
 }
